7b2de97edcbaf005eb9d86a00d6b12f47a5bba39
Max Leiter <mleiter18@priorypanther.com>
2015-12-16 09:30:20 -0800

    Further optimizations

--- bindings.c
+++ bindings.c
@@ -67,23 +67,21 @@ void draw_window(SCREEN *screen, const char *title, unsigned char window_flags) 
 
 unsigned char prompt_string(SCREEN *screen, char *buffer, unsigned short buffer_length, const char *prompt_string) __naked {
 	__asm
-	POP DE 
-	POP AF 
-	POP IX 
-	POP BC 
-	POP HL 
-	PUSH HL 
-	PUSH BC 
+	POP DE
+	POP AF
+	POP IX
+	POP BC
+	POP HL
+	PUSH HL
+	PUSH BC
 	PUSH IX
-	PUSH AF 
-	PUSH IY
 	PUSH AF
-	POP IY 
-	RST 0x10 
-	.DB _CORELIB_ID 
-	CALL _CORELIB_PROMPTSTRING 
+	EX (SP), IY 
+		RST 0x10 
+		.DB _CORELIB_ID 
+		CALL _CORELIB_PROMPTSTRING
 	LD L, A 
-	POP IY 
+	EX (SP),IY 
 	PUSH DE 
 	RET
 	__endasm;
